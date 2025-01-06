d1b2978235d01dfddc41d58ef24ffab52cf3abea
charnelledev <kengnemaiva54@gmail.com>
2025-01-06 16:55:46 +0100

    lecon01-javascript

---
+++ script.js
@@ -0,0 +1,18 @@
+// let nbre1 =12;
+// console.log(typeof(nbre1));
+// let nbre2 =5;
+// let nbre3 =15;
+// let lets = "Eva"
+// console.log(typeof(lets));
+// console.log(lets + ' a '+ nbre1 + 'ans')
+
+console.log("somme de deux nombre")
+let nbre1 =  12
+let nbre2 = 5
+let somme = nbre1 + nbre2
+console.log('la somme est de' + somme);
+console.log('la somme  de '+  nbre1 + ' et  de ' + nbre2 + ' est de ' +somme)
+console.log("soustraction de deux nombres ")
+
+// var resultat = nombre1 + nombre2;
+// nombre1 = nbre1-nbre3;
