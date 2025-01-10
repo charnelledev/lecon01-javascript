9b782bcffa7351339119cfcfeee09af08e66d7cc
charnelledev <kengnemaiva54@gmail.com>
2025-01-10 15:31:08 +0100

    LECON01-JAVASCRIPT

--- script.js
+++ script.js
@@ -6,13 +6,69 @@
 // console.log(typeof(lets));
 // console.log(lets + ' a '+ nbre1 + 'ans')
 
-console.log("somme de deux nombre")
-let nbre1 =  12
-let nbre2 = 5
-let somme = nbre1 + nbre2
-console.log('la somme est de' + somme);
-console.log('la somme  de '+  nbre1 + ' et  de ' + nbre2 + ' est de ' +somme)
-console.log("soustraction de deux nombres ")
+// console.log("somme de deux nombre")
+// let nbre1 =  12
+// let nbre2 = 5
+// let somme = nbre1 + nbre2
+// console.log('la somme est de' + somme);
+// console.log('la somme  de '+  nbre1 + ' et  de ' + nbre2 + ' est de ' +somme)
+// console.log("soustraction de deux nombres ")
 
 // var resultat = nombre1 + nombre2;
 // nombre1 = nbre1-nbre3;
+
+
+// let nbre02 =  parseInt (prompt("entrez le premier nombre"));
+// let nbre03 = parseInt(prompt(" entrez le deuxieme nombre"));
+// let som1 = parseInt(nbre02 / nbre03);
+
+// alert("la somme est de " +som1);
+
+// alert('la somme ${nbre02} /${nbre03} = ${som1} ')
+// console.log(som1);
+
+// alert("bonjour alert");
+// prompt("bonjour prompt");
+// document.write("bonjour document")
+
+
+let age = 25;
+console.log(age);
+
+if(age >= 18){
+    console.log("majeur");
+}else{
+    console.log("mineur");
+}
+let majeur = age >= 18 ? "majeur" : "mineur";
+
+
+som = 0;
+a = 0;
+i = 0
+ som2 = som + 0;
+i = 1 
+som2 = som + 1;
+i = 2 
+som2 = som + 2;
+i = 3 
+som2 = som + 3;
+i = 4 
+som2 = som + 4;
+
+console.log(som2);
+
+som1 = 0
+for (let i = 0; i <= 4; i++) {
+    // som = som + i;
+    som1 += i;
+console.log(`${som1} `);
+
+}
+
+
+const nombre =7;
+for(let i = 0; i <=12; i++){
+    const result =nombre*i;
+    console.log(`${nombre} x ${i} = ${result} \n`);
+}
